d5273f5c56650160b75122c2cbd9f421cf061c13
Pavel Lukoyanov <148583866+complagaet@users.noreply.github.com>
2025-11-26 06:09:14 +0500

    feat: update metadata

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -7,11 +7,29 @@ import SearchProvider from '@/src/contexts/SearchContext';
 
 export const metadata: Metadata = {
     title: 'Bügın’ Tenders',
-    description: '...',
+    description:
+        'Найдите товары, надёжных поставщиков, честные цены и актуальные объявления — всё в одном умном поиске.',
     icons: {
-        icon: '/favicon.ico',
-        shortcut: '/favicon.ico',
+        icon: '/favicon.png',
+        shortcut: '/favicon.png',
         apple: '/apple-touch-icon.png',
+    },
+    openGraph: {
+        title: 'Bügın’ Tenders',
+        description:
+            'Найдите товары, надёжных поставщиков, честные цены и актуальные объявления — всё в одном умном поиске.',
+        url: 'https://bugintenders.lukoyanov.love',
+        siteName: 'Bügın’ Tenders',
+        images: [
+            {
+                url: '/og-image.png', // путь к твоей OG-картинке
+                width: 1200,
+                height: 630,
+                alt: 'Bügın’ Tenders',
+            },
+        ],
+        locale: 'ru_RU',
+        type: 'website',
     },
 };
 

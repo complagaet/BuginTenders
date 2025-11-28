60c6901d71a3112d1daa6ce749cbff97897cb30e
hypertonika <160002060+hypertonika@users.noreply.github.com>
2025-11-28 05:30:51 +0500

    fix: index addition

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -12,6 +12,7 @@ import UserRoutes from './routes/User.js';
 import SearchRoutes from './routes/ProductSearch.js';
 import SupplierRoutes from './routes/SupplierSearch.js';
 import rnuCheckRouter from "./routes/rnuCheck.js";
+import AnnounceRoutes from './routes/AnnounceSearch.js'
 
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
@@ -42,6 +43,7 @@ app.use(
 app.use('/api', AuthRoutes);
 app.use('/api', SearchRoutes);
 app.use('/api', SupplierRoutes);
+app.use('/api', AnnounceRoutes);
 app.use("/api/rnu", rnuCheckRouter);
 app.use('/api', checkToken, AdminRoutes);
 app.use('/api', checkToken, UserRoutes);
